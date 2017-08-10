63b732e6b12e9c698a0a475ee0a040af9649118e
Diego Ferigo <diego.ferigo@iit.it>
2017-08-10 09:16:25 +0000

    First working version of DiscreteFilter block with 1D I/O signal

---
+++ toolbox/include/DiscreteFilter.h
@@ -0,0 +1,44 @@
+#include "Block.h"
+#include <string>
+
+#ifndef WBT_FILTER_H
+#define WBT_FILTER_H
+
+namespace wbt {
+    class DiscreteFilter;
+} // namespace wbt
+
+namespace iCub {
+    namespace ctrl {
+        class IFilter;
+    }
+} // namespace iCub
+
+namespace yarp {
+    namespace sig {
+        class Vector;
+    }
+} // namespace yarp
+
+class wbt::DiscreteFilter : public wbt::Block {
+private:
+    iCub::ctrl::IFilter* filter;
+    yarp::sig::Vector* num;
+    yarp::sig::Vector* den;
+
+    static void stringToYarpVector(const std::string s, yarp::sig::Vector* v);
+
+public:
+    static std::string ClassName;
+
+    DiscreteFilter();
+    ~DiscreteFilter() = default;
+
+    virtual unsigned numberOfParameters();
+    virtual bool configureSizeAndPorts(BlockInformation* blockInfo, wbt::Error* error);
+    virtual bool initialize(BlockInformation* blockInfo, wbt::Error* error);
+    virtual bool terminate(BlockInformation* blockInfo, wbt::Error* error);
+    virtual bool output(BlockInformation* blockInfo, wbt::Error* error);
+};
+
+#endif // WBT_FILTER_H
