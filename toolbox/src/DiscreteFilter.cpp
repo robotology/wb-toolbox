1ff1fb0e78909b12f7dfc98076c71926bdeff65e
Diego Ferigo <diego.ferigo@iit.it>
2017-08-11 11:54:49 +0000

    DiscreteFilter: Included parameters for initializing input / output
    Kept as private variables only what is strictly required

--- toolbox/src/DiscreteFilter.cpp
+++ toolbox/src/DiscreteFilter.cpp
@@ -17,6 +17,9 @@
 #define PARAM_IDX_1LOWP_FC 4 // ::FirstOrderLowPassFilter cut frequency
 #define PARAM_IDX_1LOWP_TS 5 // ::FirstOrderLowPassFilter sampling time
 #define PARAM_IDX_MD_ORDER 6 // ::MedianFilter order
+#define PARAM_IDX_INIT_Y0 7  // Output initialization
+#define PARAM_IDX_INIT_U0 8  // ::Filter input initialization
+
 // Inputs
 #define INPUT_IDX_SIGNAL 0
 // Outputs
@@ -30,15 +33,15 @@ using namespace yarp::sig;
 
 std::string DiscreteFilter::ClassName = "DiscreteFilter";
 
-DiscreteFilter::DiscreteFilter() : firstRun(true), filter(nullptr), inputSignalVector(nullptr)
+DiscreteFilter::DiscreteFilter() : filter(nullptr), inputSignalVector(nullptr)
 {
-    num = new Vector(0);
-    den = new Vector(0);
+    y0 = new Vector(0);
+    u0 = new Vector(0);
 }
 
 unsigned DiscreteFilter::numberOfParameters()
 {
-    return 6;
+    return 8;
 }
 
 bool DiscreteFilter::configureSizeAndPorts(BlockInformation* blockInfo, wbt::Error* error)
@@ -92,6 +95,8 @@ bool DiscreteFilter::initialize(BlockInformation* blockInfo, wbt::Error* error)
     std::string filter_type;
     std::string num_coeff_str;
     std::string den_coeff_str;
+    std::string y0_str;
+    std::string u0_str;
     wbt::Data firstOrderLowPassFilter_fc;
     wbt::Data firstOrderLowPassFilter_ts;
     wbt::Data medianFilter_order;
@@ -104,7 +109,9 @@ bool DiscreteFilter::initialize(BlockInformation* blockInfo, wbt::Error* error)
     // Get the string parameter
     if (!(blockInfo->getStringParameterAtIndex(PARAM_IDX_FLT_TYPE, filter_type)
           && blockInfo->getStringParameterAtIndex(PARAM_IDX_NUMCOEFF, num_coeff_str)
-          && blockInfo->getStringParameterAtIndex(PARAM_IDX_DENCOEFF, den_coeff_str))) {
+          && blockInfo->getStringParameterAtIndex(PARAM_IDX_DENCOEFF, den_coeff_str)
+          && blockInfo->getStringParameterAtIndex(PARAM_IDX_INIT_Y0, y0_str)
+          && blockInfo->getStringParameterAtIndex(PARAM_IDX_INIT_U0, u0_str))) {
         if (error) {
             error->message = ClassName + " Failed to parse string parameters.";
         }
@@ -112,8 +119,12 @@ bool DiscreteFilter::initialize(BlockInformation* blockInfo, wbt::Error* error)
     }
 
     // Convert the strings from the Matlab syntax ("[1.0 2 3.33]") to yarp::sig::Vector
+    yarp::sig::Vector num(0);
+    yarp::sig::Vector den(0);
     stringToYarpVector(num_coeff_str, num);
     stringToYarpVector(den_coeff_str, den);
+    stringToYarpVector(y0_str, *y0);
+    stringToYarpVector(u0_str, *u0);
 
     // Create the filter object
     // ========================
@@ -121,13 +132,13 @@ bool DiscreteFilter::initialize(BlockInformation* blockInfo, wbt::Error* error)
     // Generic
     // -------
     if (filter_type == "Generic") {
-        if (num->length() == 0 || den->length() == 0) {
+        if (num.length() == 0 || den.length() == 0) {
             if (error) {
                 error->message = ClassName + " (Generic) Wrong coefficients size";
             }
             return 1;
         }
-        filter = new Filter(*num, *den);
+        filter = new Filter(num, den);
     }
     // FirstOrderLowPassFilter
     // -----------------------
@@ -162,6 +173,31 @@ bool DiscreteFilter::initialize(BlockInformation* blockInfo, wbt::Error* error)
         return false;
     }
 
+    // Get the width of the input vector
+    inputSignalWidth = blockInfo->getInputPortWidth(INPUT_IDX_SIGNAL);
+
+    // Check the initial conditions are properly sized
+    unsigned outputSignalWidth = blockInfo->getInputPortWidth(OUTPUT_IDX_SIGNAL);
+    unsigned y0Width           = y0->length();
+    unsigned u0Width           = u0->length();
+
+    if (y0Width != outputSignalWidth) {
+        if (error) {
+            error->message = ClassName + " y0 and output signal sizes don't match";
+        }
+        return false;
+    }
+
+    if ((filter_type == "Generic") && (u0Width != inputSignalWidth)) {
+        if (error) {
+            error->message = ClassName + " (Generic) u0 and input signal sizes don't match";
+        }
+        return false;
+    }
+
+    // Allocate the input signal
+    inputSignalVector = new Vector(inputSignalWidth, 0.0);
+
     return true;
 }
 
@@ -175,19 +211,45 @@ bool DiscreteFilter::terminate(BlockInformation* blockInfo, wbt::Error* error)
         filter = nullptr;
     }
 
-    if (num) {
-        delete num;
-        num = nullptr;
-    }
-
-    if (den) {
-        delete den;
-        den = nullptr;
-    }
-
     if (inputSignalVector) {
         delete inputSignalVector;
         inputSignalVector = nullptr;
+    }
+
+    if (y0) {
+        delete y0;
+        y0 = nullptr;
+    }
+
+    if (u0) {
+        delete u0;
+        u0 = nullptr;
+    }
+
+    return true;
+}
+
+bool DiscreteFilter::initializeInitialConditions(BlockInformation* blockInfo, wbt::Error* error)
+{
+    // Reminder: this function is called when, during runtime, a block is disabled
+    // and enabled again. The method ::initialize instead is called just one time.
+
+    // If the initial conditions for the output are not set, allocate a properly
+    // sized vector
+    if (*y0 == Vector(1, 0.0)) {
+        y0 = new Vector(inputSignalWidth, 0.0);
+    }
+
+    // Initialize the filter. This is required because if the signal is not 1D,
+    // the default filter constructor initialize a wrongly sized y0
+    // Moreover, the Filter class has a different constructor that handles the
+    // zero-gain case
+    Filter* filter_c = dynamic_cast<Filter*>(filter);
+    if (filter_c != nullptr) {
+        filter_c->init(*y0, *u0);
+    }
+    else {
+        filter->init(*y0);
     }
 
     return true;
@@ -200,20 +262,6 @@ bool DiscreteFilter::output(BlockInformation* blockInfo, wbt::Error* error)
     // Get the input and output signals
     Signal inputSignal  = blockInfo->getInputPortSignal(INPUT_IDX_SIGNAL);
     Signal outputSignal = blockInfo->getOutputPortSignal(OUTPUT_IDX_SIGNAL);
-
-    unsigned inputSignalWidth = blockInfo->getInputPortWidth(INPUT_IDX_SIGNAL);
-
-    // Allocate the memory for the input data
-    if (firstRun) {
-        // Allocate the input signal
-        inputSignalVector = new Vector(inputSignalWidth, 0.0);
-
-        // Initialize the filter. This is required because if the signal is not 1D,
-        // the default filter constructor initialize a wrongly sized y0
-        filter->init(Vector(inputSignalWidth));
-
-        firstRun = false;
-    }
 
     // Copy the Signal to the data structure that the filter wants
     for (unsigned i = 0; i < inputSignalWidth; ++i) {
@@ -229,10 +277,8 @@ bool DiscreteFilter::output(BlockInformation* blockInfo, wbt::Error* error)
     return true;
 }
 
-void DiscreteFilter::stringToYarpVector(const std::string str, Vector* v)
+void DiscreteFilter::stringToYarpVector(const std::string str, Vector& v)
 {
-    assert(v != nullptr);
-
     std::string s = str;
 
     // Lambda expression used to remove "[]," carachters
@@ -252,5 +298,5 @@ void DiscreteFilter::stringToYarpVector(const std::string str, Vector* v)
     float f;
 
     while (sstrm >> f)
-        v->push_back(f);
+        v.push_back(f);
 }
