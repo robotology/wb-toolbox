82b760ba3a00fa12de55047d1b25e6ba514a23fb
Diego Ferigo <diego.ferigo@iit.it>
2017-11-08 16:20:42 +0000

    Matlab functions used by Blocks which inherit from WBBlock class
    - BlockInitialization:
      Find the Config block hierarchically closer to the caller block
    
    - Mask2WBToolboxConfig:
      Read the mask of a config block and generates from it a WBToolboxConfig object, validating also its data
    
    - WBToolboxConfig2Mask:
      Read a WBToolboxConfig object and populates the mask of a Config block

---
+++ +WBToolbox/Mask2WBToolboxConfig.m
@@ -0,0 +1,39 @@
+function [ WBTConfig ] = Mask2WBToolboxConfig(configBlock)
+%MASK2WBTOOLBOXCONFIG Summary of this function goes here
+%   Detailed explanation goes here
+
+defaultString = '''NonValid''';
+
+if (...
+    strcmp(char(get_param(configBlock,'RobotName')),defaultString) && ...
+    strcmp(char(get_param(configBlock,'UrdfFile')),defaultString) && ...
+    strcmp(char(get_param(configBlock,'LocalName')),defaultString) && ...
+    strcmp(char(get_param(configBlock,'GravityVector')),defaultString) && ...
+    strcmp(char(get_param(configBlock,'ControlledJoints')),defaultString) && ...
+    strcmp(char(get_param(configBlock,'ControlBoardsNames')),defaultString) ...
+    )
+    error('[Mask2WBToolboxConfig] The provided config is NonValid.');
+end
+
+WBTConfig = WBToolbox.WBToolboxConfig;
+
+WBTConfig.RobotName = stripApices(char(get_param(configBlock,'RobotName')));
+WBTConfig.UrdfFile  = stripApices(char(get_param(configBlock,'UrdfFile')));
+WBTConfig.LocalName = stripApices(char(get_param(configBlock,'LocalName')));
+
+ControlledJointsChar = stripApices(char(get_param(configBlock,'ControlledJoints')));
+WBTConfig.ControlledJoints = eval('base',ControlledJointsChar);
+
+ControlBoardsNamesChar = stripApices(char(get_param(configBlock,'ControlBoardsNames')));
+WBTConfig.ControlBoardsNames = eval('base',ControlBoardsNamesChar);
+
+GravityVectorChar = stripApices(char(get_param(configBlock,'GravityVector')));
+WBTConfig.GravityVector = eval('base',GravityVectorChar);
+
+end
+
+function str = stripApices(str)
+    if (strcmp(str(1),'''') && strcmp(str(end),''''))
+        str = str(2:end-1);
+    end
+end
