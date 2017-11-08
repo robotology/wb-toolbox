520266f1097cdadf4f25ad21c5755a2ed30e23e7
Diego Ferigo <diego.ferigo@iit.it>
2017-11-08 15:39:05 +0000

    SimulatorSynchronizer refactored

--- toolbox/include/SimulatorSynchronizer.h
+++ toolbox/include/SimulatorSynchronizer.h
@@ -7,27 +7,31 @@ namespace wbt {
     class SimulatorSynchronizer;
 }
 
-class wbt::SimulatorSynchronizer : public wbt::Block {
+class wbt::SimulatorSynchronizer : public wbt::Block
+{
 public:
-    static std::string ClassName;
+    static const std::string ClassName;
 
     SimulatorSynchronizer();
-    virtual ~SimulatorSynchronizer();
+    ~SimulatorSynchronizer() override = default;
 
-    virtual unsigned numberOfParameters();
-    virtual std::vector<std::string> additionalBlockOptions();
-    virtual bool configureSizeAndPorts(BlockInformation *blockInfo, wbt::Error *error);
-    virtual bool initialize(BlockInformation *blockInfo, wbt::Error *error);
-    virtual bool terminate(BlockInformation *blockInfo, wbt::Error *error);
-    virtual bool output(BlockInformation *blockInfo, wbt::Error *error);
+    unsigned numberOfParameters() override;
+    std::vector<std::string> additionalBlockOptions() override;
+    bool configureSizeAndPorts(BlockInformation* blockInfo) override;
+    bool initialize(BlockInformation* blockInfo) override;
+    bool terminate(BlockInformation* blockInfo) override;
+    bool output(BlockInformation* blockInfo) override;
 
 private:
-
     double m_period;
     bool m_firstRun;
 
     struct RPCData;
-    RPCData *m_rpcData;
+    RPCData* m_rpcData;
+
+    static const unsigned PARAM_PERIOD;     // Period
+    static const unsigned PARAM_GZCLK_PORT; // Gazebo clock port
+    static const unsigned PARAM_RPC_PORT;   // RPC client port name
 };
 
 #endif /* end of include guard: WBT_SIMULATORSYNCHRONIZER_H */
