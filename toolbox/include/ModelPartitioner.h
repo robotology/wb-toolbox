028dbd603947979aded515f44cfa3bed1b41903c
Diego Ferigo <diego.ferigo@iit.it>
2017-11-08 15:55:03 +0000

    YARPWBIConverter refactored and renamed to ModelPartitioner

---
+++ toolbox/include/ModelPartitioner.h
@@ -0,0 +1,33 @@
+#ifndef WBT_MODELPARTITIONER_H
+#define WBT_MODELPARTITIONER_H
+
+#include "WBBlock.h"
+#include "RobotInterface.h"
+#include <vector>
+#include <memory>
+
+namespace wbt {
+    class ModelPartitioner;
+}
+
+class wbt::ModelPartitioner : public wbt::WBBlock
+{
+private:
+    bool m_yarp2WBI;
+
+    std::shared_ptr<JointsMapString> m_jointsMapString;
+
+public:
+    static const std::string ClassName;
+    ModelPartitioner() = default;
+    ~ModelPartitioner() override = default;
+
+    unsigned numberOfParameters() override;
+
+    bool configureSizeAndPorts(BlockInformation* blockInfo) override;
+    bool initialize(BlockInformation* blockInfo) override;
+    bool terminate(BlockInformation* blockInfo) override;
+    bool output(BlockInformation* blockInfo) override;
+};
+
+#endif /* end of include guard: WBT_MODELPARTITIONER_H */
