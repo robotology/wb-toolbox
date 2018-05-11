58fbc3cd2ce793a08c60637698f3afe0264d5a36
Diego Ferigo <diego.ferigo@iit.it>
2018-05-11 14:20:58 +0200

    BlockInformation::optionFromKey is now a pure virtual method

--- toolbox/src/base/BlockInformation.cpp
+++ toolbox/src/base/BlockInformation.cpp
@@ -9,8 +9,3 @@
 #include "BlockInformation.h"
 
 const std::string wbt::BlockOptionPrioritizeOrder = "wbt.BlockOptionPrioritizeOrder";
-
-bool wbt::BlockInformation::optionFromKey(const std::string& /*key*/, double& /*option*/) const
-{
-    return false;
-}
